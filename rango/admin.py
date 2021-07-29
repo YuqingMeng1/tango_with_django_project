050414445e93855621f3b0b0bfcd759b2a507041
Yuqing Meng <2538877M@student.gla.ac.uk>
2021-07-29 16:43:28 +0800

    Commit the final code of chapter 6

--- rango/admin.py
+++ rango/admin.py
@@ -3,8 +3,9 @@ from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
- # Add in this class to customise the Admin Interface
+
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+
 admin.site.register(Category, CategoryAdmin)
- 
+admin.site.register(Page, PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -4,18 +4,17 @@ from rango.models import Category
 from rango.models import Page
 
 def index(request):
-    # Query the database for a list of ALL categories currently stored.
-    # Order the categories by the number of likes in descending order.
-    # Retrieve the top 5 only -- or all if less than 5.
-    # Place the list in our context_dict dictionary (with our boldmessage!)
-    # that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
 
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
+    context_dict['extra'] = 'From the model solution on GitHub'
 
     return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     return render(request, 'rango/about.html')
