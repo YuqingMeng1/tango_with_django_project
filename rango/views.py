39dc336817f536b13eb2bfb6fafef7ed7f8d9021
Yuqing Meng <2538877M@student.gla.ac.uk>
2021-07-29 15:59:37 +0800

    modify admin.py/views.py/models.py

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
-
+from rango.models import Page
 
 def index(request):
     # Query the database for a list of ALL categories currently stored.
@@ -19,4 +19,20 @@ def index(request):
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['pages'] = None
+        
+    
+    return render(request, 'rango/category.html', context=context_dict)
 
